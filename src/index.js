43434628af2a362247d9c11904a84913a4375ef4
matheussgsilva <matheussgsilva@hotmail.com>
2023-03-23 21:31:12 -0300

    Starts folder architecture using the ITCSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/settings/colors.css';
+import './styles/generic/reset.css';
+import './styles/elements/base.css';
 import App from './App';
-import './index.css';
 
 ReactDOM.render(
   <App />,
