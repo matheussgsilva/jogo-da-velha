c91bc6f8bce093f4d98faed913ca623140417d0b
matheussgsilva <matheussgsilva@hotmail.com>
2023-03-26 20:56:23 -0300

    Delete styles from Logo and change the src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import About from './objects/About';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <About />
+      <Header />
     );
   }
 }
--- src/objects/Logo/index.js
+++ src/objects/Logo/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import LogoCollab from '../../images/logo.png';
 import './styles.css';
 
 const Logo = () => {
     return (
-        <img src="../../images/logo.png" />
+        <img src={LogoCollab} alt="Logo CollabCode" />
     )
 }
 
