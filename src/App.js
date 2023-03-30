1032cfeaf3bede861e7c95a02f42402543e77717
matheussgsilva <matheussgsilva@hotmail.com>
2023-03-29 22:06:04 -0300

    Create the Card object and update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './components/Header';
+import Card from './objects/Card';
 
-class App extends Component {
-  render() {
+function App() {
     return (
+      <div>
       <Header />
+      <Card />
+      </div>
     );
-  }
 }
 
 export default App;
---
+++ src/objects/Card/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <article className="card">Card</article>
+    )
+}
+
+export default Card
